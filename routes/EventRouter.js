cb7b3e172587ec52ef762d860f76a5956870fb7c
David Martel <daveymartel@gmail.com>
2020-12-29 18:23:39 -0500

    event services

--- routes/EventRouter.js
+++ routes/EventRouter.js
@@ -1,13 +1,12 @@
 const Router = require("express").Router();
 const controller = require("../controllers/EventController");
 
-Router.post("/create", controller.CreateEvent)
-Router.get("/all", controller.GetAllEvents)
-Router.get("/:event_id", controller.GetOneEvent)
-Router.get("/user/:user_id", controller.GetEventsByUser)
-Router.get("/friend/:friendId/", controller.GetEventsOfFriends)
-Router.put("/:event_id", controller.EditEvent)
-Router.delete("/:event_id", controller.DeleteEvent)
+Router.post("/create", controller.CreateEvent);
+Router.get("/all", controller.GetAllEvents);
+Router.get("/:event_id", controller.GetOneEvent);
+Router.get("/user/:user_id", controller.GetEventsByUser);
+Router.get("/friend/:user_id/", controller.GetEventsOfFriends);
+Router.put("/:event_id", controller.EditEvent);
+Router.delete("/:event_id", controller.DeleteEvent);
 
-
-module.exports = Router
+module.exports = Router;
