11b5a933fba20917e1b1eaf7eb138825e009a936
David Martel <daveymartel@gmail.com>
2020-12-29 12:40:49 -0500

    model setup

---
+++ migrations/20201229173613-create-alerts.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Alerts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      noti_id: {
+        type: Sequelize.INTEGER
+      },
+      msg: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Alerts');
+  }
+};
