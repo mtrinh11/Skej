13c46d915474051fda205c6f158f25ce0f682e7f
Jady Hom <hyjady@gmail.com>
2020-12-30 17:08:45 -0500

    setup reducers

--- client/src/store/reducers/FriendReducer.js
+++ client/src/store/reducers/FriendReducer.js
@@ -1,7 +1,31 @@
-import {} from "../types";
+import {
+  ADD_FRIEND,
+  GET_ALL_FRIENDS,
+  GET_FRIEND,
+  REMOVE_FRIEND,
+} from "../types";
 
-const initialState = {};
+const initialState = {
+  friends: [],
+  newFriend: "",
+};
 
-const FriendReducer = (state = initialState, action) => {};
+const FriendReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_FRIEND:
+      return { ...state, friends: [...state.friends], newFriend: "" };
+    case GET_ALL_FRIENDS:
+      return { ...state, friends: action.payload };
+    case GET_FRIEND:
+      return { ...state, friends: action.payload };
+    case REMOVE_FRIEND:
+      return {
+        ...state,
+        friends: [...state.friends.filter((friend, index) => index !== action.payload)],
+      };
+    default:
+      return { ...state };
+  }
+};
 
 export default FriendReducer;
