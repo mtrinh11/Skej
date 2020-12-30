2f37e5fcc9676c893bee3d62b53cdfc80a2101bf
mtrinhwincomp <mtrinh11@gmail.com>
2020-12-30 15:18:11 -0800

    add signin error tag

--- client/src/store/reducers/EventReducer.js
+++ client/src/store/reducers/EventReducer.js
@@ -2,6 +2,11 @@ import {} from "../types";
 
 const initialState = {};
 
-const EventReducer = (state = initialState, action) => {};
+const EventReducer = (state = initialState, action) => {
+    switch (action.type){
+        default:
+            return {...state}
+    }
+};
 
 export default EventReducer;
