88a60a622058dc791a7631d8550052014ed48c68
Jady Hom <hyjady@gmail.com>
2020-12-30 15:17:37 -0500

    setup all actions

--- client/src/store/actions/FriendsAction.js
+++ client/src/store/actions/FriendsAction.js
@@ -1,2 +1,60 @@
-import {} from "../types";
-import {} from "../../services/FriendsServices";
+import {
+  ADD_FRIEND,
+  GET_ALL_FRIENDS,
+  GET_FRIEND,
+  REMOVE_FRIEND,
+} from "../types";
+import {
+  __AddFriend,
+  __GetAllFriends,
+  __GetFriend,
+  __RemoveFriend,
+} from "../../services/FriendsServices";
+
+export const addFriend = (userId, friendId) => async (dispatch) => {
+  try {
+    let res = await __AddFriend(userId, friendId);
+    dispatch({
+      type: ADD_FRIEND,
+      payload: res,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getAllFriends = (id) => async (dispatch) => {
+  try {
+    let res = await __GetAllFriends(id);
+    dispatch({
+      type: GET_ALL_FRIENDS,
+      payload: res,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getFriend = (id) => async (dispatch) => {
+  try {
+    let res = await __GetFriend(id);
+    dispatch({
+      type: GET_FRIEND,
+      payload: res,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const removeFriend = (userId, friendId) => async (dispatch) => {
+  try {
+    let res = await __RemoveFriend(userId, friendId);
+    dispatch({
+      type: REMOVE_FRIEND,
+      payload: res,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
--- client/src/store/actions/RequestActions.js
+++ client/src/store/actions/RequestActions.js
@@ -1,2 +1,29 @@
-import {} from "../types";
-import {} from "../../services/RequestsServices";
+import { CREATE_REQUEST, DELETE_REQUEST } from "../types";
+import {
+  __CreateRequest,
+  __DeleteRequest,
+} from "../../services/RequestsServices";
+
+export const createRequest = (userId, requestedId) => async (dispatch) => {
+  try {
+    let res = await __CreateRequest(userId, requestedId);
+    dispatch({
+      type: CREATE_REQUEST,
+      payload: res,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteRequest = (userId, requestedId) => async (dispatch) => {
+  try {
+    let res = await __DeleteRequest(userId, requestedId);
+    dispatch({
+      type: DELETE_REQUEST,
+      payload: res,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
